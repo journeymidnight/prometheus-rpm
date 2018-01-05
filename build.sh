c6fb5443ebb1c53ddfff0b2941475856129ff09b
Dongmao Zhang <deanraccoon@gmail.com>
2018-01-05 15:23:40 +0800

    Add build.sh and .drone.yml

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-NAMES=( alertmanager graphite_exporter jmx_exporter node_exporter prometheus pushgateway )
+NAMES=(alertmanager ceph_exporter node_exporter prometheus )
 
 for NAME in "${NAMES[@]}"; do
     VERSION=$(cat "$NAME/VERSION")
     echo "Building $NAME in version $VERSION"
     cd $NAME && rm -rf rpmbuild && make && cd ..
+done
+
+find . -name "*.centos.x86_64.rpm"|grep -v sysvinit|while read line do 
+	cp $line .
 done
 
